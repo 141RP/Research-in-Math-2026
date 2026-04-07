1dfdfe8febaaad9e7fe0659fbb6bcc3acc29d5ff
Rohin Patel <to.rohinpatel@gmail.com>
2026-04-07 09:47:21 -0400

    Layer update - fwd/back prop

--- layer.py
+++ layer.py
@@ -1,12 +1,33 @@
+import numpy as np
+
 class Layer:
     def __init__(self):
         self.input = None
         self.output = None
+        self.weights = None
+        self.bias = None
 
     def forward(self, input):
-        # TODO: return output
-        pass
+        # Implementation based on referential linear forward pass: Z = W.dot(X) + b
+        self.input = input
+        self.output = np.dot(self.weights, self.input) + self.bias 
+        return self.output
 
     def backward(self, output_gradient, learning_rate):
-        # TODO: update parameters and return input gradient
-        pass
+        # m = number of training examples in the batch
+        m = self.input.shape[1] if len(self.input.shape) > 1 else 1
+
+        # Gradients calculation based on the user's provided math:
+        # dW = 1 / m * dZ.dot(X.T)
+        weights_gradient = (1 / m) * np.dot(output_gradient, self.input.T)
+        # db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+        bias_gradient = (1 / m) * np.sum(output_gradient, axis=1, keepdims=True)
+        
+        # Gradient passed back to previous layer: W.T.dot(dZ)
+        input_gradient = np.dot(self.weights.T, output_gradient)
+
+        # Update parameters 
+        self.weights -= learning_rate * weights_gradient
+        self.bias -= learning_rate * bias_gradient
+
+        return input_gradient
